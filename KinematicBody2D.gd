031bb0988441d4d09e505218e09375c1f378fd69
DESKTOP-NV4V396\tablet1 <mrnannings@gmail.com>
2018-06-17 22:28:35 +0200

    part 2;

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -8,21 +8,25 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	
-	if Input.is_key_pressed(KEY_ESCAPE):
-		get_tree().quit()
-		
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
 		motion.x = 0
+		$Sprite.play("Idle")
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	else:
+		$Sprite.play("Jump")
 	
 	motion = move_and_slide(motion, UP)
 	
---
+++ World.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+
+func _process(delta):
+	
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
